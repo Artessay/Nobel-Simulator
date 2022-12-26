5bafe41b2dbced3c641de3bb365439bcc5b637cb
Artessay <rihong.edu@outlook.com>
2022-12-26 14:14:14 +0800

    看上去还行的阴影

--- res/shaders/point_shadows.fs
+++ res/shaders/point_shadows.fs
@@ -58,13 +58,14 @@ float ShadowCalculation(vec3 fragPos)
     return shadow;
 }
 
+
 void main()
 {           
     vec3 color = texture(diffuseTexture, fs_in.TexCoords).rgb;
     vec3 normal = normalize(fs_in.Normal);
     vec3 lightColor = vec3(0.3);
     // ambient
-    vec3 ambient = 0.3 * lightColor;
+    vec3 ambient = 0.6 * lightColor;
     // diffuse
     vec3 lightDir = normalize(lightPos - fs_in.FragPos);
     float diff = max(dot(lightDir, normal), 0.0);
